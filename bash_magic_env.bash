808f7952154332f42b178ac3e3e98306ff864c89
Brent Sanders <git@thoughtnoise.net>
2012-09-11 10:28:30 -0700

    a lot of cleanup; consolidated vars to a single hash to spam ENV a bit less

--- bash_magic_env.bash
+++ bash_magic_env.bash
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-[[ -v MAGIC_ENV_ACTIVE ]] || declare -A MAGIC_ENV_ACTIVE=()
+[[ -v MAGIC_ENV        ]] || declare -A MAGIC_ENV
+[[ -v MAGIC_ENV_ACTIVE ]] || declare -A MAGIC_ENV_ACTIVE
+
+# [[ -n "${MAGIC_ENV[SHOW_CHANGES]}" ]] && MAGIC_ENV[SHOW_CHANGES]=true
+# [[ -n "${MAGIC_ENV[VERBOSE]}"      ]] && MAGIC_ENV[VERBOSE]=true
+# [[ -n "${MAGIC_ENV[LOADER]}"       ]] && MAGIC_ENV[LOADER]=".magic_env"
+# [[ -n "${MAGIC_ENV[UNLOADER]}"     ]] && MAGIC_ENV[UNLOADER]="${MAGIC_ENV[LOADER]}.unload"
+
+me_set() { [[ -z "${MAGIC_ENV[${1}]}" ]] && MAGIC_ENV[${1}]="$2" ; };
+me_set SHOW_CHANGES true
+me_set VERBOSE      true
+me_set LOADER       ".magic_env"
+me_set UNLOADER     "${MAGIC_ENV[LOADER]}.unload"
+unset me_set
 
 _magic_env_listdef() {
     read -r -d '' script <<'EOF'
@@ -22,49 +35,55 @@ _magic_env_newdefs() {
 
 _magic_env_load() {
     local old="${1}" new="${2}"
-    local cur env_vars env="${new}/.env"
-    if ! [[ -v MAGIC_ENV_ACTIVE[${new}] ]] && [[ -r  "${env}" ]] ; then
-        # only if new (no double loading)
-        # only if the local environment file is present
+    local loader="${new}/${MAGIC_ENV[LOADER]}"
 
-        cur="$(_magic_env_listdef)";
-        . "${env}"
-        env_vars="$(_magic_env_newdefs "${cur}")"
-
-        echo "$(echo "${env_vars}" | wc -w) vars loaded form: ${env}"
-        declare -p ${env_vars}
-
-        MAGIC_ENV_ACTIVE[${new}]=${env_vars}
+    if [[ -n "${MAGIC_ENV_ACTIVE[${new}]}" ]] ; then
+        return    # only if new (no double loading)
     fi
 
+    if ! [[ -r  "${loader}" ]] ; then
+        return    # only if the local environment file is present
+    fi
+
+    local cur env_vars local
+    cur="$(_magic_env_listdef)";
+    . "${loader}"
+    env_vars="$(_magic_env_newdefs "${cur}")"
+    len="$(echo "${env_vars}" | wc -w)"
+
+    ${MAGIC_ENV[VERBOSE]}      && echo "loaded ${len} vars form: ${loader}"
+    ${MAGIC_ENV[SHOW_CHANGES]} && declare -p ${env_vars}
+
+    MAGIC_ENV_ACTIVE[${new}]=${env_vars}
 }
 
 _magic_env_unload() {
     local old="${1}" new="${2}"
-    local vars="${MAGIC_ENV_ACTIVE[${old}]}"
-    if [[ -n "${vars}" ]] ; then
-        echo "UNLOAD: ${old}"
-        echo unset ${vars}
-        unset ${vars}
-        MAGIC_ENV_ACTIVE[${old}]=
+    [[ -z "${old}" ]] && return
+
+    local var="${MAGIC_ENV_ACTIVE[${old}]}"
+    [[ -z "${var}" ]] && return
+
+    echo "UNLOAD: ${old}"
+    local unloader="${old}/${MAGIC_ENV[UNLOADER]}"
+    [[ -r "${unloader}" ]] && . "${unloader}"
+
+    ${MAGIC_ENV[SHOW_CHANGES]} && echo unset ${var}
+    unset ${var}
+
+    MAGIC_ENV_ACTIVE[${old}]=
+}
+
+_magic_env_update() {
+    local pwd="$PWD"
+    if [[ "${pwd}" != "${MAGIC_ENV[PWD]}" ]]; then
+        _magic_env_unload "${MAGIC_ENV[PWD]}" "${pwd}"
+        _magic_env_load   "${MAGIC_ENV[PWD]}" "${pwd}"
+        MAGIC_ENV[PWD]="${pwd}"
     fi
 }
 
-_magic_env_chdir() {
-    local pwd="${1}"
-    _magic_env_unload "${MAGICENV_PWD}" "${pwd}"
-    _magic_env_load "${MAGICENV_PWD}" "${pwd}"
-    MAGICENV_PWD="${pwd}"
-}
-
-function _magic_env_update() {
-    local dir="$PWD"
-    if [[ "$dir" != "${MAGICENV_PWD}" ]]; then
-        _magic_env_chdir "$PWD"
-    fi
-}
-
-function cd() {
+cd() {
     if command cd "${@}" ; then
         _magic_env_update
         return 0
@@ -72,6 +91,12 @@ function cd() {
         return $?
     fi
 }
+
+
+# finally, update it once for the current directory
+_magic_env_update
+
+
 
 # Local Variables:
 # mode: sh
